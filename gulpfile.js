3626fdeb77930e4584f2f79a53b4b2bfdff6617e
Twiknight <twiknight@163.com>
2015-10-07 20:11:03 +0800

    fixed to match directories in src

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 
 gulp.task("compile",function(){
-    return gulp.src("src/*.js")
+    return gulp.src("src/**/*.js")
       .pipe(babel())
       .pipe(gulp.dest("lib"));
 });
